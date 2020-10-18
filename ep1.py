a4617db2c36f3b6d10e7437560f753df65c6bf6a
Carlos Inacio <cinacio@eusou.space>
2020-10-18 16:43:35 -0300

    - Mudanças para a entrada verificada - Implementação de apostas - Implementação de limpeza do terminal - Melhorias para a seleção de terceira carta e "display" das mãos

--- ep1.py
+++ ep1.py
@@ -5,6 +5,10 @@
 # enumeração de estados de jogo
 from enum import Enum
 import random
+import os
+
+def limpar_terminal():
+    os.system('cls||clear')
 
 class ESTADOS(Enum):
     MENU = 0
@@ -18,7 +22,7 @@ fichas_iniciais = 20
 
 # todas as cartas possíveis e seus valores
 cartas_individuais = {"A" : 1, "2" : 2, "3" : 3, "4" : 4, "5" : 5, "6" : 6, "7" : 7, "8" : 8, "9" : 9, "10" : 0, "J" : 0, "Q" : 0, "K" : 0}
-
+apostas = ["Jogador", "Banco", "Empate"]
 comissao_da_casa = { "Jogador" : [1.29, 1.29, 1.29, 1.29, 1.29, 1.24, 1.24, 1.24], "Banco" : [1.01, 1.01, 1.01, 1.01, 1.01, 1.06, 1.06, 1.06], "Empate" : [15.75, 15.75, 15.75, 15.75, 15.75, 14.44, 14.44, 14.36] }
 
 # variáveis 'globais' que podem ser resetadas caso o usuário queria
@@ -30,40 +34,38 @@ fichas = []
 finalizar_execucao = False
 
 # função para criar input de int e limpar o código poupando-o de ter as mesmas verficações várias vezes
-def entrada_verificada(out, ret_string):
+def entrada_verificada(out, err, rejeitar_negativo):
     # out - string imprimido pelo input
-    # ret_string - espera-se que um string seja retornado
+    # err - string de texto caso o número seja negativo
+    # rejeitar negativo - rejeita números negativos
     retorno = -1
-    entrada = input(out)
-    if ret_string == True:
-        return entrada
-    elif ret_string == False and entrada.isdigit() == True:
-        retorno = int(entrada)
-    else:
-        print("Entrada inválida!")
-    
+    while 1:
+        entrada = input(out)
+        if entrada.isdigit() == True:
+            retorno = int(entrada)
+            if retorno <= 0 and rejeitar_negativo:
+                print(err)
+            else: 
+                return retorno
+        else:
+            print("Entrada inválida!")
     return retorno
 
+
+
 def multipla_escolha(escolhas):
+    print("\n")
     indice = 1
     for e in escolhas:
         print("{0} - {1}".format(indice, e))
         indice += 1
     
-    entrada = -1
-    while entrada <= 0:
-        entrada = entrada_verificada("O que deseja fazer? ", False)
-        if entrada <= 0:
-            print("Entrada inválida!")
+    entrada = entrada_verificada("O que deseja fazer? ", "Entrada inválida!", True)
     
     return entrada
 
 def cadastro_jogadores(jogadores, fichas):
-    quantidade_jogadores = 0
-    while quantidade_jogadores <= 0:
-        quantidade_jogadores = entrada_verificada("Quantos jogadores irão jogar? ", False)
-        if quantidade_jogadores <= 0:
-            print("Deve haver ao menos um jogador!")
+    quantidade_jogadores = entrada_verificada("Quantos jogadores irão jogar? ", "Deve haver ao menos um jogador!", True)
 
     for j in range(quantidade_jogadores):
         jogadores.append(input("Nome do jogador {0}: ".format(j + 1)))
@@ -84,16 +86,18 @@ def menu():
     # cadastro de jogadores
     cadastro_jogadores(jogadores, fichas)
 
-    entrada_baralhos = 0
-    while entrada_baralhos <= 0:
-        entrada_baralhos = entrada_verificada("Com quantos baralhos deseja jogar? ", False)
-        if entrada_baralhos <= 0:
-            print("Deve haver ao menos um baralho!")
+    entrada_baralhos = entrada_verificada("Com quantos baralhos deseja jogar? ", "Deve haver ao menos um baralho!", True)
 
     baralho *= int(entrada_baralhos)
 
+    limpar_terminal()
+
+    print("Jogadores:")
+    for j in jogadores:
+        print("  {0}".format(j))
+    print("Baralho: {0} cartas, ou {1} baralhos".format(len(baralho), int(len(baralho) / 52)))
+
     escolha = multipla_escolha(["Continuar", "Reconfigurar", "Sair"])
-    print(escolha)
     if escolha == 1:
         estado = ESTADOS.EMJOGO
     elif escolha == 2:
@@ -105,7 +109,7 @@ def carta_aleatoria():
     return baralho[random.randint(0, len(baralho) - 1)]
 
 def terceira_carta(mao):
-    soma = mao[0] + mao[1]
+    soma = cartas_individuais[mao[0]] + cartas_individuais[mao[1]]
 
     # pegar carta aleatória
     # aqui pegamos a chave e não o dicionário
@@ -134,18 +138,41 @@ def distribuir_cartas():
     return mao
 
 def imprimir_mao(mao):
+    soma = 0
     mostrar_chaves = ""
     mostrar_valores = ""
+
     for c in mao:
         mostrar_chaves += "   {0}".format(c)
         mostrar_valores += "   {0}".format(cartas_individuais[c])
+        soma += cartas_individuais[c]
+
+    soma = soma - 10 if soma > 10 else soma
+
     print(mostrar_chaves)
     print(mostrar_valores)
+    print("Soma: {0}".format(soma))
 
 def realizar_aposta():
     global baralho, jogadores, fichas, finalizar_execucao, estado
     
-    print("Distribuindo cartas...")    
+    limpar_terminal()
+
+    # aposta = [ [ valor, tipo ], [ valor, tipo ]  ]
+    # matriz com uma lista de "valor, tipo" para cada jogador
+    aposta = []
+    for j in range(len(jogadores)):
+        _aposta = [entrada_verificada("Quanto deseja apostar {0}? ".format(jogadores[j]), "Você tem que apostar um valor válido!", True)]
+        _aposta.append(apostas[multipla_escolha(["Jogador", "Banco", "Empate"]) - 1])
+        aposta.append(_aposta)
+
+    limpar_terminal()
+    for j in range(len(jogadores)):
+        print("Aposta de {0}: {1} fichas em {2}".format(jogadores[j], aposta[j][0], aposta[j][1]))
+
+    print("Distribuindo cartas...")
+    input("Pressione enter para continuar")
+    limpar_terminal()
     
     mao_banco = distribuir_cartas()
     mao_jogadores = []
@@ -164,12 +191,13 @@ def realizar_aposta():
     imprimir_mao(mao_banco)
     print("--------------------------")
     
-
     
     
 
 def jogo():
     global baralho, jogadores, fichas, finalizar_execucao, estado
+
+    limpar_terminal()
 
     for j in range(len(jogadores)):
         print("{0}: {1} fichas".format(jogadores[j], fichas[j]))
