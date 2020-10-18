4149f5b282202bb33f6f12a7e8de12ae6ee56e3e
Carlos Inacio <cinacio@eusou.space>
2020-10-18 14:36:31 -0300

    - Múltiplos baralhos implementados - Adicionada verificação de entrada

--- ep1.py
+++ ep1.py
@@ -15,18 +15,51 @@ estado = ESTADOS.MENU
 # essa variável é utilizada posteriormente para todos os jogadores cadastrados
 fichas_iniciais = 20
 
+# função para criar input de int e limpar o código poupando-o de ter as mesmas verficações várias vezes
+def entrada_verificada(out, ret_string):
+    # out - string imprimido pelo input
+    # ret_string - espera-se que um string seja retornado
+    retorno = -1
+    entrada = input(out)
+    if ret_string == True:
+        return entrada
+    elif ret_string == False and entrada.isdigit() == True:
+        retorno = int(entrada)
+    else:
+        print("Entrada inválida!")
+    
+    return retorno
+
 def cadastro_jogadores(jogadores, fichas):
-    quantidade_jogadores = int(input("Quantos jogadores vão jogar? "))
+    quantidade_jogadores = 0
+    while quantidade_jogadores <= 0:
+        quantidade_jogadores = entrada_verificada("Quantos jogadores irão jogar? ", False)
+        if quantidade_jogadores <= 0:
+            print("Deve haver ao menos um jogador!")
+
     for j in range(quantidade_jogadores):
         jogadores.append(input("Nome do jogador {0}: ".format(j + 1)))
         fichas.append(fichas_iniciais)
 
 
 # todas as cartas possíveis e seus valores
-cartas = {"A" : 1, "2" : 2, "3" : 3, "4" : 4, "5" : 5, "6" : 6, "7" : 7, "8" : 8, "9" : 9, "10" : 0, "J" : 0, "Q" : 0, "K" : 0}
+cartas_individuais = {"A" : 1, "2" : 2, "3" : 3, "4" : 4, "5" : 5, "6" : 6, "7" : 7, "8" : 8, "9" : 9, "10" : 0, "J" : 0, "Q" : 0, "K" : 0}
+
+# definir um baralho completo de 52 cartas (13 cartas por naipe)
+baralho = []
+for c in cartas_individuais.keys():
+    for i in range(4):
+        baralho.append(c)
 
 # cadastro de jogadores
 jogadores = []
 fichas = []
 cadastro_jogadores(jogadores, fichas)
 
+entrada_baralhos = 0
+while entrada_baralhos <= 0:
+    entrada_baralhos = entrada_verificada("Com quantos baralhos deseja jogar? ", False)
+    if entrada_baralhos <= 0:
+        print("Deve haver ao menos um baralho!")
+
+baralho *= int(entrada_baralhos)
